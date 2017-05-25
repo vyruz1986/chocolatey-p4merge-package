a2013dedbc07924a4a31576c5cf1d3738f39d6cf
Stechschulte, Adam M <astechschulte@paychex.com>
2017-05-24 23:53:21 -0400

    Add 64bit installer is gone.

--- P4Merge/tools/chocolateyinstall.ps1
+++ P4Merge/tools/chocolateyinstall.ps1
@@ -4,38 +4,31 @@ $ErrorActionPreference = 'Stop';
 $packageName= 'P4Merge'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://filehost.perforce.com/perforce/r17.1/bin.ntx86/p4vinst.exe'
+$url64bit   = 'http://filehost.perforce.com/perforce/r17.1/bin.ntx64/p4vinst64.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
+  url64bit      = $url64bit
 
   softwareName  = 'Helix Visual*'
 
   checksum      = '41fc6ca913311e1f8875709bb2508654'
+  checksum64    = '2911a8c56ab946b5c13ddafaf0ce1428'
   checksumType  = 'md5'
+  checksumType64= 'md5'
 
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/s /V"/qn ADDLOCAL=P4MERGE,QT,QT32"'
 }
 
+$forceX86 = $env:chocolateyForceX86;
+if (-not $forceX86) {
+  if (Get-OSArchitectureWidth 64) {
+    $packageArgs.silentArgs = '/s /V"/qn ADDLOCAL=P4MERGE,QT,QT64"'
+  }
+}
+
 Install-ChocolateyPackage @packageArgs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

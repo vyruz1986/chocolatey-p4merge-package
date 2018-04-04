4f07867f76dbe56240a7d5c39904ada84c8435ee
hansnull <t-len@gmx.de>
2018-04-04 10:10:54 +0200

    Update to 2018.1.1637591

--- P4Merge/tools/chocolateyinstall.ps1
+++ P4Merge/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'P4Merge'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://filehost.perforce.com/perforce/r17.2/bin.ntx86/p4vinst.exe'
-$url64bit   = 'http://filehost.perforce.com/perforce/r17.2/bin.ntx64/p4vinst64.exe'
+$url        = 'http://filehost.perforce.com/perforce/r18.1/bin.ntx86/p4vinst.exe'
+$url64bit   = 'http://filehost.perforce.com/perforce/r18.1/bin.ntx64/p4vinst64.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,10 +13,10 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64bit
 
-  softwareName  = 'Helix Visual*'
+  softwareName  = 'Helix Core*'
 
-  checksum      = '9338c28b411c975106d6ec91cb514efe16f56d93d08ab113677309d62af31236'
-  checksum64    = 'f055dfac50c3484529dce8c73e0338e0850b749f71bfb90d5c5ded6bcb64589d'
+  checksum      = '6696A5A9690B33875E84139F25E912E29CE5C5E87CE1D7A0C1D9841A3326CD6B'
+  checksum64    = 'EEA0E8BE023EBEE5AC0DF69A1285F7ED0F19AF98AEFB08C808FC484674FCE1C2'
   checksumType  = 'SHA256'
   checksumType64= 'SHA256'
 
@@ -27,7 +27,7 @@ $packageArgs = @{
 $forceX86 = $env:chocolateyForceX86;
 if (-not $forceX86) {
   if (Get-OSArchitectureWidth 64) {
-    $packageArgs.silentArgs = '/s /V"/qn ADDLOCAL=P4MERGE,QT,QT64"'
+    $packageArgs.silentArgs = '/s /V"/qn ADDLOCAL=P4MERGE,QT"'
   }
 }
 
